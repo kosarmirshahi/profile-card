95fcdfa0fc4d424a5ae30837d7c58578f2361191
kosar mirshahi <kosarmirshahi@gmail.com>
2023-09-21 15:20:41 +0330

    compelete card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import pic from "./assets/pic.JPG";
+const skills = [
+  {
+    skill: "Html",
+    color: "#CA42AF",
+    level: "Advance",
+  },
+  {
+    skill: "Css",
+    color: "#878787",
+    level: "Advance",
+  },
+  {
+    skill: "JavaScript",
+    color: "#CA42AF",
+    level: "Intermediate",
+  },
+  {
+    skill: "React",
+    color: "#878787",
+    level: "Basic",
+  },
+];
+function Header() {
+  return <img src={pic} alt="pic" className="headerImg" />;
+}
 
+function Description() {
+  return (
+    <div>
+      <h2>Kosar Mirshahi</h2>
+      <p>
+        I am 22 years old. I have a bachelor's degree in computer engineering. I
+        am a master's student in artificial intelligence. I am a front-end
+        programmer with two years of work experience. I am currently learning
+        React. My skills:
+      </p>
+    </div>
+  );
+}
+function Skill() {
+  return (
+    <div>
+      {skills.map((skill) => (
+        <div className="skills" style={{ backgroundColor: skill.color }}>
+          <p>{skill.skill}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Description />
+      <Skill />
     </div>
   );
 }
